346978e01c29d66b7ca0c3f9b5fdf922c15270b2
Joao Vitor <jvitorsilvadn@gmail.com>
2025-07-25 11:38:45 -0300

    adicionando uma nova funcao

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 let imagem = document.querySelector(".copo-starbucks");
 let cor = document.querySelector(".circulo");
 
-function trocarImagem(dadoImg, dadoCor) {
+function trocarImagem(dadoImg) {
     imagem.src = dadoImg;
+}
+
+function trocarCor(dadoCor) {
     cor.style.background = dadoCor;
 }
